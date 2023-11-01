cde4c4e4f96fbb3b266d17d55a17a0e27c078477
Giousned <guille_ranza@hotmail.com>
2023-11-01 10:00:49 +0000

    [ADD] Creando repo con la solucion y algunas explicaciones

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# Proyect_Data_Engineer_Challenge
-Juego de encontrar los movimientos mínimos de una piedra de 1x3 para llegar al extremo inferior derecho de un array de 2 dimensiones
+# Proyect_Data_Engineer_Challenge:
+
+Juego de encontrar los movimientos mínimos de una piedra de 1x3 para llegar al extremo inferior derecho de un array de 2 dimensiones.
+
+# COMO EJECUTAR EL ARCHIVO APP.PY:
+RUN IN TERMINAL: -> python /workspace/Proyect_Data_Engineer_Challenge/src/app.py
+OR: -> python /workspace/Proyect_Data_Engineer_Challenge/src/app.py > output.txt
+
+
+# Descripcion del reto:
+
+Hello, future Damavis teammate! First of all, thanks for accepting our challenge. You
+are free to use any programming language from among Scala, Python or Java.
+Here’s the challenge to evaluate how you code, so please do not use ChatGPT or
+similar, not even to get ideas about which algorithm to develop. Trust us when we
+say that we are capable of discerning between code generated using concepts from
+ChatGPT and code produced through independent development. Send us your own
+code, thought and written by yourself without any help from anyone. We value the
+presence of documentation regarding the utilized code structure, the implemented
+algorithm, and instructions on its execution.
+Try not to use any framework or external library, use only the base language
+capabilities. We value readable code, structure, tests, good code principles and best
+practices. You are free to choose the programming code paradigm and architecture
+that you think that fits best. Therefore, aim to avoid coding in Jupyter Notebooks or
+in a single file without any structure.
+If you cannot finish the challenge, please send us your partial solution anyway.
+It’s preferably you to create a Version Control System (VCS) repository. Git could be
+a good choice to track your commits, and easily share your results with us. Although,
+if you are not familiar with VCS, simply send us a zip file containing your code.
+
+# EXPLICACION DEL JUEGO:
+
+The goal is to carry the rod from the top left corner of the labyrinth to the bottom
+right corner. This rod is not exactly the lightest thing you can imagine, so the
+participant would naturally want to do it as fast as possible.
+Find the minimal number of moves required to carry the rod through the labyrinth.
+The labyrinth can be represented as a rectangular matrix, some cells of which are
+marked as blocked, and the rod can be represented as a 1 × 3 rectangle. The rod
+can't collide with the blocked cells or the walls, so it's impossible to move it into a
+position in which one of its cells coincides with the blocked cell or the wall. The goal
+is thus to move the rod into position in which one of its cells is in the bottom right
+cell of the labyrinth.
+There are 5 types of moves that the participant can perform: move the rod one cell
+down or up, to the right or to the left, or to change its orientation from vertical to
+horizontal and vice versa. The rod can only be rotated about its center, and only if the
+3 × 3 area surrounding it is clear from the obstacles or the walls.
+The rod is initially positioned horizontally, and its left cell lies in [0, 0].
+
+
+# Ejemplos:
+
+![Alt text](imagen.png)
+
+
+# Condiciones iniciales y finales:
+
+![Alt text](imagen-1.png)
+
+
+
+# TESTS de Ejemplos:
+
+![Alt text](imagen-2.png)
---
+++ src/app.py
@@ -0,0 +1,146 @@
+# Importamos random para crear nuestros propios arrays
+import random
+
+# Elegimos el alto y el ancho que queremos que en este caso nos lo dan en el enunciado los valores máximos y mínimos
+alto = random.randint(3, 6)
+ancho = random.randint(3, 10)
+
+array = []
+labyrinth = []
+
+
+# Creamos nuestro array aleatorio cada vez para jugar nosotros
+def two_dimensional_array(alto, ancho):
+
+    for i in range(alto):
+
+        array = []
+
+        for j in range(ancho):
+            
+            numero_aleatorio = random.randint(0, 15)
+            
+            if (numero_aleatorio == 7):
+                array.append("#")
+            else:
+                array.append(".")
+        
+        labyrinth.append(array)
+        
+    return labyrinth
+
+
+# Creamos la funcion para buscar los movimientos minimos para llegar al extremo inferior derecho del array
+def go_corner_right(labyrinth):
+    
+    ancho = len(labyrinth[0])
+    alto = len(labyrinth)
+
+    counter = 0
+    horizontal = True
+    vertical = False
+
+    # Aqui usamos "guardas/cortafuegos" para que si ocurre alguno de estos 2 casos salga directamente de la funcion e indique que no hay solucion posible
+    if (labyrinth[-1][-1] == "#"):
+        counter = -1
+        return "solution(labyrinth) = " + str(counter)
+    
+    if (labyrinth[0][0] == "#" or labyrinth[0][1] == "#"  or labyrinth[0][2] == "#"):
+        counter = -1
+        return "solution(labyrinth) = " + str(counter)
+    
+    
+    # Si llegamos hasta aquí ya buscamos los movimientos minimos que hacen falta para llegar, dividimos la busqueda en 2 casos, cuando la longitud del array es 3 o cuando es superior. 
+    # Este primer caso es cuando es superior a 3. Que hay que moverse horizontalmente y verticalmente y con cambios de horientacion.    
+    if (ancho>3):
+        
+        j = 3
+        
+        for i in range(0, alto - 1):
+            
+            while (j < ancho):
+
+                if (labyrinth[i][j] != "#"):                            # Buscamos y vamos moviendo horizontalmente primero, y cuando no podemos bajamos 1 vertical si es posible
+                    counter += 1
+                    j += 1
+                elif (labyrinth[i+1][j-2] != "#" and labyrinth[i+1][j-1] != "#" and labyrinth[i+1][j] != "#"):          # Aqui se baja 1 vertical si se puede
+                    counter += 1
+                    i += 1
+                else:
+                    counter = -1
+                    return "solution(labyrinth) = " + str(counter)
+            
+            if (labyrinth[i+1][j-3] != "#" and labyrinth[i+1][j-2] != "#" and labyrinth[i+1][j-1] != "#" and horizontal):               # Cuando ya estamos en el borde derecho ya solo queda bajar verticalmente
+                counter += 1
+            
+            elif (i >= 2):
+                if (horizontal):                            # Aqui hacemos una orientacion de horizontal a vertical de la piedra, al comprobar que hemos bajado 2 lineas verticalmente y con lo cual hay una superficie de 3x3 libre alrededor de la piedra
+                    counter += 1                            # Sumamos un movimiento por el cambio de horientacion solo cuando lo hacemos
+                horizontal = False
+                vertical = True
+                
+                if(labyrinth[i+1][j-1] != "#" and vertical):            # Seguimos buscando verticalmente pero con la horientacion vertical tambien
+                    counter += 1
+                else:
+                    counter = -1
+                    return "solution(labyrinth) = " + str(counter)              # Devolvemos la solucion del menor numero de movimientos
+            else:
+                counter = -1
+                return "solution(labyrinth) = " + str(counter)
+
+
+
+    else:
+        for i in range(0, alto - 1):                                        # Y aquí es exactamente igual pero solo con busqueda vertical desde el principio ya que el array solo tiene una longitud de 3 y la piedra no puede moverse horizontalmente en horizontal
+            
+            if (labyrinth[i+1][0] != "#" and labyrinth[i+1][1] != "#" and labyrinth[i+1][2] != "#" and horizontal):
+                counter += 1
+            
+            elif (i >= 2):
+                if (horizontal):
+                    counter += 1
+                horizontal = False
+                vertical = True
+                
+                if(labyrinth[i+1][j-1] != "#" and vertical):
+                    counter += 1
+                else:
+                    counter = -1
+                    return "solution(labyrinth) = " + str(counter)
+            
+            else:
+                counter = -1
+                return "solution(labyrinth) = " + str(counter)
+
+    
+    return "solution(labyrinth) = " + str(counter)
+                
+        
+
+print("labyrinth = ", two_dimensional_array(alto, ancho))               # Aqui imprimimos en pantalla nuestro array aleatorio creado para jugar
+
+print(go_corner_right(labyrinth))                                       # Aqui se imprime los movimientos minimos que hacen falta
+
+
+###########                                                         # Aqui comprobamos con una serie de test que nos dan de ejemplo/prueba y se pueden añadir más para comprobar nuestra solucion
+## TESTS ##
+###########
+
+labyrinth_test1 = [[".",".",".",".",".",".",".",".","."],["#",".",".",".","#",".",".",".","."],[".",".",".",".","#",".",".",".","."],[".","#",".",".",".",".",".","#","."],[".","#",".",".",".",".",".","#","."]]
+
+print("Solution Test 1", go_corner_right(labyrinth_test1))
+
+
+labyrinth_test2 = [[".",".",".",".",".",".",".",".","."],["#",".",".",".","#",".",".","#","."],[".",".",".",".","#",".",".",".","."],[".","#",".",".",".",".",".","#","."],[".","#",".",".",".",".",".","#","."]]
+
+print("Solution Test 2", go_corner_right(labyrinth_test2))
+
+
+labyrinth_test3 = [[".",".","."],[".",".","."],[".",".","."]]
+
+print("Solution Test 3", go_corner_right(labyrinth_test3))
+
+
+labyrinth_test4 = [[".",".",".",".",".",".",".",".",".","."],[".","#",".",".",".",".","#",".",".","."],[".","#",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".","."],[".","#",".",".",".",".",".",".",".","."],[".","#",".",".",".","#",".",".",".","."],[".",".",".",".",".",".","#",".",".","."],[".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".","."]]
+
+print("Solution Test 4", go_corner_right(labyrinth_test4))
